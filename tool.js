a369b7255d4a1ec0ff78018d1f1ddac01682269b
Hosiang1026 <hosiang1026@sina.com>
2019-10-10 11:14:56 +0800

    初始化图库

--- tool.js
+++ tool.js
@@ -1,58 +1,101 @@
 "use strict";
 var fs = require("fs");
 var date = require("silly-datetime");
-var path = "./resources/image/";
+var arr = [];
+var albumObjwww = {};
+var components = [];
 
-fs.readdir(path, function (err, files) {
-    if (err) {
-        return;
+const files = fs.readdirSync('./resources');
+files.forEach(function (item) {
+    var stat = fs.lstatSync("./resources/" + item);
+    if (stat.isDirectory() === true) {
+        components.push(item)
     }
+});
 
-    var arr = [];
-    var albumObjwww = {};
-    var photosArr = [];
+console.log(components);
 
-    //"抱歉，你没有权限访问";
-    (function iterator(index) {
-        if (index == files.length) {
-            //fs.writeFile("./photos.json", JSON.stringify(arr, null, "\t"));
-            console.log(JSON.stringify(arr, null, "\t"));
-            fs.writeFile("./resources/photos.json", JSON.stringify(arr, null, "\t"), function (err) {
-            if (err) throw err;
-             console.log('write photos.json success!');
-            });
+/**
+ * 遍历文件夹
+ */
+for(var j = 0,len=components.length; j < len; j++) {
+    var value = components[j];
+    var urlPath = "/photos/" +value+ "/";
+    var path = "./resources/"+value+"/";
+    console.log(path);
 
-            return;
-        }
-        fs.stat(path + files[index], function (err, stats) {
-            if (err) {
-                return;
-            }
-            if (stats.isFile()) {
-
-                var photo = {};
-                var thumbnail = files[index];
-                //npm install silly-datetime
-                var today = date.format(new Date(),'YYYY-MM-DD');
-
-                photo.sort = index;
-                photo.name = today + " 高清壁纸(" + index + ")";
-                photo.thumbnail = "/photos/image/"+thumbnail;
-                photo.description ="照片描述";
-
-                photosArr.push(photo);
-
-            }
-            iterator(index + 1);
-        })
-    }(0));
-
-    albumObjwww.sort = "0";
-    albumObjwww.name = "壁纸相册";
+    albumObjwww.sort = j;
+    albumObjwww.name = value+"相册";
     albumObjwww.password = "";
     albumObjwww.description = "测试相册描述";
-    albumObjwww.photos = photosArr;
 
-    arr.push(albumObjwww);
+    //读取资源文件
+    readFolder(path,urlPath,albumObjwww);
+}
 
-});
+
+/**
+ * 读取资源文件
+ * @param path
+ * @param urlPath
+ */
+function readFolder(path,urlPath,albumObjwww){
+
+    var photosArr = [];
+
+    fs.readdir(path,albumObjwww, function (err, files) {
+        if (err) {
+            return;
+        }
+
+        (function iterator(index) {
+            fs.stat(path + files[index], function (err, stats) {
+                if (err) {
+                    return;
+                }
+                if (stats.isFile()) {
+
+                    var photo = {};
+                    var thumbnail = files[index];
+                    //npm install silly-datetime
+                    var today = date.format(new Date(),'YYYY-MM-DD');
+                    photo.sort = index;
+                    photo.name = today + " 高清壁纸(" + index + ")";
+                    photo.thumbnail = urlPath + thumbnail;
+                    photo.description ="照片描述";
+
+                    photosArr.push(photo);
+
+                }
+                iterator(index + 1);
+
+            });
+
+            if (index == files.length) {
+                albumObjwww.photos = photosArr;
+                arr.push(albumObjwww);
+                if (arr.length == components.length){
+                    //写入Json文件
+                    writeJsonFile(arr);
+                }
+            }
+        }(0));
+
+    });
+}
+
+/**
+ * 写入Json文件
+ */
+function writeJsonFile(arr) {
+
+    //fs.writeFile("./photos.json", JSON.stringify(arr, null, "\t"));
+    console.log(JSON.stringify(arr, null, "\t"));
+    fs.writeFile("./resources/photos.json", JSON.stringify(arr, null, "\t"), function (err) {
+        if (err) throw err;
+        console.log('write photos.json success!');
+    });
+
+
+}
+
